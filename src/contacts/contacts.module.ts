ba6bcc3f576165884c4e72bba52e78a3d231dea4
Emmett Walsh <emmettwalsh@gmail.com>
2022-06-14 16:36:17 +0100

    import sequelize modules

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { ContactsResolver } from './contacts.resolver';
+import { Contact } from './entities/contact.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Contact])], // the forFeature() method defines which models are registered
   providers: [ContactsResolver, ContactsService]
 })
 export class ContactsModule {}
