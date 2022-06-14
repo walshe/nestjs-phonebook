2f60ec032e1fbdd824786aa5ec451125f980e4eb
Emmett Walsh <emmettwalsh@gmail.com>
2022-06-14 17:34:41 +0100

    doc

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -4,6 +4,8 @@ import { ContactsResolver } from './contacts.resolver';
 import { Contact } from './entities/contact.entity';
 import { SequelizeModule } from '@nestjs/sequelize';
 
+//providers - the providers that will be instantiated by the Nest injector and that may be shared at least across this module - a provider is injected as a dependancy to this module
+//imports - the list of imported modules that export the providers which are required in this module
 @Module({
   imports: [SequelizeModule.forFeature([Contact])], // the forFeature() method defines which models are registered
   providers: [ContactsResolver, ContactsService]
