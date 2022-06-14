58706c710e196308ec96dec5f3a950eb99473d5d
Emmett Walsh <emmettwalsh@gmail.com>
2022-06-14 18:26:18 +0100

    add nestjs 'guards' to all protected queries/mutations that check for a valid jwt

--- src/contacts/contacts.resolver.ts
+++ src/contacts/contacts.resolver.ts
@@ -1,4 +1,6 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { GqlAuthGuard } from 'src/auth/graphql-guard';
 import { ContactsService } from './contacts.service';
 import { CreateContactInput } from './dto/create-contact.input';
 import { PageRequestInput } from './dto/pageRequest.input';
@@ -9,26 +11,31 @@ export class ContactsResolver {
   constructor(private readonly contactsService: ContactsService) {}
 
   @Mutation('createContact')
+  @UseGuards(GqlAuthGuard)
   create(@Args('createContactInput') createContactInput: CreateContactInput) {
     return this.contactsService.create(createContactInput);
   }
 
   @Query('contacts')
+  @UseGuards(GqlAuthGuard)
   findAll(@Args('pageRequestInput') pageRequestInput: PageRequestInput) {
     return this.contactsService.findAll(pageRequestInput);
   }
 
   @Query('contact')
+  @UseGuards(GqlAuthGuard)
   findOne(@Args('id') id: number) {
     return this.contactsService.findOne(id);
   }
 
   @Mutation('updateContact')
+  @UseGuards(GqlAuthGuard)
   update(@Args('updateContactInput') updateContactInput: UpdateContactInput) {
     return this.contactsService.update(updateContactInput.id, updateContactInput);
   }
 
   @Mutation('removeContact')
+  @UseGuards(GqlAuthGuard)
   remove(@Args('id') id: number) {
     return this.contactsService.remove(id);
   }
